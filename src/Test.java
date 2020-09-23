bbdb8df83ad27df289f010cb7b33de757468ff63
zyt-12345 <1797017516@qq.com>
2020-09-23 15:16:26 +0800

    修改后的代码

---
+++ src/Test.java
@@ -0,0 +1,15 @@
+/**
+ * @author zyt
+ * @description 测试代码
+ * @date 2020/9/23 15:10
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        add(2,4);
+    }
+
+    private static Integer add(int num,int num2) {
+        return num+num2;
+    }
+}
